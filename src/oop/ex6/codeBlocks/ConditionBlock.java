c11ebb55e5d2608ec7d372baacecef20503dcea9
weitzshaked <weitzshaked@gmail.com>
2017-06-15 16:27:38 +0300

    hi

--- src/oop/ex6/codeBlocks/ConditionBlock.java
+++ src/oop/ex6/codeBlocks/ConditionBlock.java
@@ -3,38 +3,36 @@ package oop.ex6.codeBlocks;
 import oop.ex6.Exceptions.LogicalException;
 import oop.ex6.variables.Variables;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Condition Block Class
  */
 public class ConditionBlock extends CodeBlock {
 
-    private String condition;
+    private String[] conditions;
     private String type;
 
-    public static final String conditionPattern = "true|false|(\\d+(\\.\\d+)?)";
+    public static final String conditionPattern = "(true|false|(\\d+(\\.\\d+)?)";
 
 
     /**
-     * creates a new condition block
+     * creates a new conditionsLine block
      *
-     * @param parent    block
-     * @param codeLines inner code lines
-     * @param condition block's condition
-     * @param type      if/ while block
+     * @param parent         block
+     * @param codeLines      inner code lines
+     * @param conditionsLine block's conditionsLine
+     * @param type           if/ while block
      * @throws Exception
      */
-    public ConditionBlock(CodeBlock parent, String[] codeLines, String condition, String type) throws Exception {
+    public ConditionBlock(CodeBlock parent, String[] codeLines, String conditionsLine, String type) throws Exception {
         super(parent, codeLines);
         this.type = type;
-        if (checkOneLiner(condition, conditionPattern)) {
-            this.condition = condition;
-        } else if (Variables.canAssign(condition.trim(), parent, "boolean")) {
-            this.condition = condition;
-        } else {
-            throw new LogicalException();
+        this.conditions = conditionsLine.split("(\\|{2})|(&{2})");
+        for (String condition : conditions) {
+            if (!checkOneLiner(condition, conditionPattern) ||
+                    !Variables.canAssign(condition, parent, "boolean")) {
+                throw new LogicalException();
+            }
         }
     }
 }
--- src/oop/ex6/codeBlocks/Method.java
+++ src/oop/ex6/codeBlocks/Method.java
@@ -7,10 +7,11 @@ public class Method extends CodeBlock {
 
     private String name;
     private int paramNum = 0;
-    public static final String parameterPattern = "";
+    public static final String parameterPattern = "\\s*((?<type>\\D+)\\s(?<\\D+\\.+>))*";
 
     public Method(CodeBlock parent, String[] codeLines, String name, String parameters, String returnStatement) throws Exception{
         super(parent, codeLines);
+        this.name = name;
         //todo split parameters, create
 
         this.name = name;
